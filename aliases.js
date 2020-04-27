8835d228d2efe350c785a9fcd0c524a617999191
zer0 <matteo@dusk.network>
2020-04-27 17:24:56 +0200

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,8 @@
+var ALIASES = {};
+ALIASES["cake_rusk"] = {};
+ALIASES["lazy_static"] = {};
+ALIASES["proc_macro2"] = {};
+ALIASES["quote"] = {};
+ALIASES["rusk_contract"] = {};
+ALIASES["syn"] = {};
+ALIASES["unicode_xid"] = {};
---
+++ cake_rusk/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"attr":[["cake",""],["contract",""]]});
